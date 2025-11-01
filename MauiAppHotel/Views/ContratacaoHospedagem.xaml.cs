5e6004bdbdc8832d684bac4bf6b3eed05104cfe9
TcasT <TcasT@DESKTOP-FCQ5TRL>
2025-11-01 12:27:15 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,20 @@
+namespace MauiAppHotel.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+	public ContratacaoHospedagem()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Sobre());
+    }
+
+}
